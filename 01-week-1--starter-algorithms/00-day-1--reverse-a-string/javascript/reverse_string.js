22969556272a166d1bff6d36d04a53dd5ec6d5a3
McKayla Lankau <lankaukk@yahoo.com>
2021-10-27 15:56:16 -0400

    solved

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,9 +1,24 @@
 function reverseString(str) {
   // type your code here
+  var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: '  '");
+  console.log("=>", reverseString("  "));
+
+  console.log("");
+
+  console.log("Expecting: 'alyaKcM'");
+  console.log("=>", reverseString("McKayla"));
+
+  console.log("");
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 

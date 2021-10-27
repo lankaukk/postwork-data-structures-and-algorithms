270049646d0e0ad1753e9c36936a5c372669bdfe
McKayla Lankau <lankaukk@yahoo.com>
2021-10-27 18:15:46 -0400

    solved

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/README.md
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/README.md
@@ -1,6 +1,6 @@
 # Day 3: Fibonacci Series
 
-Find the nth element in the Fibonacci series. The Fibonacci sequence starts with a 0 followed by a 1. After that, every value is the sum of the two values preceding it. Here are the first seven values as an example: 0, 1, 1, 2, 3, 5, 8.
+Find the nth element in the Fibonacci series. The Fibonacci sequence starts with a 0 followed by a 1. After that, every value is the sum of the two values preceding it. Here are the first seven values as an example: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 
 
 ```
 Input: 0
@@ -11,7 +11,7 @@ Output: 1
 
 Input: 10
 Output: 55
-```
+``
 
 Note that we are using zero-indexing for the series.
 
--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
   // type your code here
+  if (num < 2) {
+    return num;
+  }
+
+  let arr = [0, 1]
+
+  for (let i = 0; i < num - 1; ++i) {
+    let nextNum = arr[arr.length - 2] + arr[arr.length - 1];
+    arr.push(nextNum);
+  }
+
+  return arr[arr.length - 1];
 }
 
 if (require.main === module) {
@@ -14,11 +26,43 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: 13");
+  console.log("=>", fibonacci(7));
+
+  console.log("");
+
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
+
+  console.log("");
+  console.log("=>", fibonacci(100));
 }
 
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// 10
+
+// num1 = 0
+// num2 = 1
+
+// num3 = num1 + num2
+// num4 = num2 + num3 
+
+// return num10
+
+
+// let num = 10
+
+// let arr = [0, 1]
+
+// while (arr.length < num) {
+//   arr.push = arr[arr.length - 1] + arr[arr.length - 2]
+// }
+
+// return arr[num]
+
+// until the array is as long as num, keep adding to it
+// then return the nth number

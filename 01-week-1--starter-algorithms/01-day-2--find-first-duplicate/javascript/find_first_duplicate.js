35f08f9e02e1efdca17137bb90653630fcc72fe5
McKayla Lankau <lankaukk@yahoo.com>
2021-10-27 16:23:11 -0400

    solved

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,9 +1,28 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  
+  // given an array 
+
+  let uniques = new Set();
+
+  // iterate over it
+  for (let i = 0; i < arr.length; i++) {
+    // until one number is returned twice
+    // then return that number
+    if (uniques.has(arr[i])) 
+        return arr[i];
+        uniques.add(arr[i]);
+  }
+  // otherwise, return -1
+  return -1;
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 4");
+  console.log("=>", findFirstDuplicate([2, 1, 4, 4, 3, 3, 2]));
+
+  console.log("");
+
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
